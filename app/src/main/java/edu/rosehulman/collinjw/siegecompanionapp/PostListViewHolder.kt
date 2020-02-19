dd63774dd0e267c86f053f11c10d529f362225fa
collinjw1 <collinjw@rose-hulman.edu>
2020-02-18 21:28:50 -0500

    now displaying images on post list page

--- app/src/main/java/edu/rosehulman/collinjw/siegecompanionapp/PostListViewHolder.kt
+++ app/src/main/java/edu/rosehulman/collinjw/siegecompanionapp/PostListViewHolder.kt
@@ -1,10 +1,12 @@
 package edu.rosehulman.collinjw.siegecompanionapp
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.directory_row_view.view.*
 import kotlinx.android.synthetic.main.post_row_view.view.*
 
@@ -14,41 +16,48 @@ class PostListViewHolder : RecyclerView.ViewHolder {
     val postContentTextView: TextView = itemView.post_content_text_view
     var visible: Boolean = false
     var context: Context
-    lateinit var postContent: View
+    var bitmap: Bitmap? = null
+    var firstRunCheck = true
+    lateinit var rootView: View
 
     constructor(itemView: View, adapter: PostListAdapter, context: Context): super(itemView) {
         this.context = context
-        //this.postContent = itemView.post_image
+        rootView = itemView
         itemView.setOnClickListener {
             //adapter.onPhotoSelected(adapterPosition)
             if (!visible) {
                 itemView.post_image.visibility = ImageView.VISIBLE
                 itemView.post_content_text_view.visibility = ImageView.VISIBLE
                 itemView.post_title_text_view.maxHeight = 1000
+                itemView.more_button.setImageResource(R.drawable.ic_remove_black_24dp)
             } else {
                 itemView.post_image.visibility = ImageView.GONE
                 itemView.post_content_text_view.visibility = ImageView.GONE
                 itemView.post_title_text_view.maxHeight = 200
+                itemView.more_button.setImageResource(R.drawable.ic_add_black_24dp)
             }
             visible = !visible
-            //itemView.post_image.visibility = ImageView.VISIBLE
         }
     }
 
     fun bind(post: PostObject) {
+        if (firstRunCheck) {
+            Picasso.get()
+                .load(post.mediaRef)
+                .into(rootView.post_image)
+            firstRunCheck = false
+        }
         //if statement testing for image vs video, set postContent variable
         postTitleTextView.text = post.title
         postContentTextView.text = post.textContent
-//        if (visible) {
-//            //showContent()
-//        } else {
-//            //hideContent()
-//        }
 
     }
 
-    fun showContent(post: PostObject) {
-
+    fun onImageLoaded(inComImage: Bitmap?) {
+        bitmap = inComImage
+        rootView.post_image.setImageBitmap(bitmap)
     }
+
+
 
 }
